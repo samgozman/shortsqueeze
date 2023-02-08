a09d0a5d11f6419a4abe68846ecdba2323f20618
samgozman <sam@gozman.space>
2023-02-08 23:03:52 +0400

    Modify tests

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -4,15 +4,30 @@ import shortsqueeze from '..';
 jest.setTimeout(10000);
 
 test('Should get correct parsed data', async () => {
-  const stock = (await shortsqueeze('GME'))!;
+  const stock = (await shortsqueeze('AAPL'))!;
   expect(stock).toBeDefined();
 
   // Assert that String data from the response is correct
-  expect(stock.name).toBe('Gamestop Corporation');
-  expect(stock.ticker).toBe('GME');
-
-  expect(stock.shortPercentOfFloat).toEqual(expect.any(Number));
+  expect(stock.name).toBe('Apple Inc');
+  expect(stock.ticker).toBe('AAPL');
   expect(stock.price).toEqual(expect.any(Number));
+  expect(stock.shortInterestRatioDaysToCover).toBeDefined();
+  expect(stock.shortPercentOfFloat).toBeDefined();
+  expect(stock.shortPercentIncreaseDecrease).toBeDefined();
+  expect(stock.shortInterestCurrentSharesShort).toBeDefined();
+  expect(stock.sharesFloat).toBeDefined();
+  expect(stock.shortInterestPriorSharesShort).toBeDefined();
+  expect(stock.percentFrom52WkHigh).toBeDefined();
+  expect(stock.percentFrom50DayMa).toBeDefined();
+  expect(stock.percentFrom200DayMa).toBeDefined();
+  expect(stock.percentFrom52WkLow).toBeDefined();
+  expect(stock.n52WeekPerformance).toBeDefined();
+  expect(stock.tradingVolumeTodayVsAvg).toBeDefined();
+  expect(stock.tradingVolumeToday).toBeDefined();
+  expect(stock.tradingVolumeAverage).toBeDefined();
+  expect(stock.marketCap).toBeDefined();
+  expect(stock.percentOwnedByInsiders).toBeDefined();
+  expect(stock.percentOwnedByInstitutions).toBeDefined();
 });
 
 test('Should get undefined object if the ticker is incorrect', async () => {
